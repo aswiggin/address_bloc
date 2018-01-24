ae6e1ceaf9388d99daa505b2ef2a31154ab8b902
EC2 Default User <ec2-user@ip-172-31-6-103.us-east-2.compute.internal>
2018-01-24 22:41:47 +0000

    added edit, delete, submenu methods

--- controllers/menu_controller.rb
+++ controllers/menu_controller.rb
@@ -79,8 +79,34 @@ class MenuController
             puts "New entry created"
         end
         def search_entries
+            print "Search by name: "
+            name = gets.chomp
+            match = address_book.binary_search(name)
+            system "clear"
+            if match 
+                puts match.to_s
+                search_submenu(match)
+            else
+                puts "No match found for #{name}"
+            end
         end
         def read_CSV
+            print "Enter CSV file to import: "
+            file_name = gets.chomp
+            if file_name.empty?
+                system "clear"
+                puts "No CSV file read"
+                main_menu
+            end
+            # import the specified file. then print screen and print the number of entries that were read
+            begin 
+                entry_count = address_book.import_from_csv(file_name).count
+                system "clear"
+                puts "#{entry_count} new entries added from #{file_name}"
+            rescue
+                puts "#{file_name} is not a valid CSV file, please enter the name of a valid CSV file"
+                read_CSV
+            end
         end
         def entry_submenu(entry)
             # displays submenu options
@@ -94,7 +120,10 @@ class MenuController
             case selection
                 when "n"
                 when "d"
+                    delete_entry(entry)
                 when "e"
+                    edit_entry(entry)
+                    entry_submenu(entry)
                 when "m"
                     system "clear"
                     main_menu
@@ -102,6 +131,53 @@ class MenuController
                     system "clear"
                     puts "#{selection} is not a valid input"
                     entry_submenu(entry)
+            end
+        end
+        def delete_entry(entry)
+            address_book.entries.delete(entry)
+            puts "#{entry.name} has been deleted"
+        end
+        def edit_entry(entry)
+            print "Updated name: "
+            name = gets.chomp
+            print "Updated phone number: "
+            phone_number = gets.chomp
+            print "Updated email: "
+            email = gets.chomp
+            # sets attributes to entry only if the attribute read is valid 
+            entry.name = name if !name.empty?
+            entry.phone_number = phone_number if !phone_number.empty?
+            entry.email = email if !email.empty?
+            system "clear"
+            puts "Updated entry:"
+            puts entry
+        end
+        def search_submenu(entry)
+            # #12
+            puts "\nd - delete entry"
+            puts "e - edit this entry"
+            puts "m - return to main menu"
+            # #13
+            selection = gets.chomp
+ 
+            # #14
+            case selection
+                when "d"
+                    system "clear"
+                    delete_entry(entry)
+                    main_menu
+                when "e"
+                    edit_entry(entry)
+                    system "clear"
+                    main_menu
+                when "m"
+                    system "clear"
+                    main_menu
+                else
+                    system "clear"
+                    puts "#{selection} is not a valid input"
+                    puts entry.to_s
+                    search_submenu(entry)
             end
         end
 end
